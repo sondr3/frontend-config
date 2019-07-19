e1c7be5681b4423279fe21f0f4d5c7cce11cafc4
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-07-19 22:22:28 +0200

    chore: add shared prettier config, style packages

--- packages/eslint-react/index.js
+++ packages/eslint-react/index.js
@@ -1,10 +1,6 @@
 module.exports = {
   plugins: ["react-hooks"],
-  extends: [
-    "plugin:react/recommended",
-    "prettier/react",
-    "plugin:jsx-a11y/recommended"
-  ],
+  extends: ["plugin:react/recommended", "prettier/react", "plugin:jsx-a11y/recommended"],
   rules: {
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn"
