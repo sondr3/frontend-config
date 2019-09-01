67bfb617531cbab71e1bdff226bb80b469f2a32e
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-09-01 17:08:10 +0200

    feat: remove unneeded styled-components from stylelint

--- packages/stylelint/index.js
+++ packages/stylelint/index.js
@@ -1,12 +1,6 @@
 module.exports = {
   plugins: ["stylelint-order"],
-  processors: ["stylelint-processor-styled-components"],
-  extends: [
-    "stylelint-config-recommended",
-    "stylelint-config-styled-components",
-    "stylelint-a11y/recommended",
-    "stylelint-prettier/recommended"
-  ],
+  extends: ["stylelint-config-recommended", "stylelint-a11y/recommended", "stylelint-prettier/recommended"],
   rules: {
     "order/order": ["custom-properties", "declarations"],
     "order/properties-alphabetical-order": true
