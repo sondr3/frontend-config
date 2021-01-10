627e9dcc591797d358eef40e4ba24e3e2e217d44
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-01-10 22:44:08 +0100

    feat: disable some rules from eslint-plugin-node

--- packages/eslint/src/plugins/node.js
+++ packages/eslint/src/plugins/node.js
@@ -2,4 +2,11 @@ module.exports = {
   env: { node: true },
   plugins: ["node"],
   extends: ["plugin:node/recommended-module"],
+  rules: {
+    // not needed because of the `import` plugin
+    "node/no-missing-require": "off",
+    "node/no-missing-import": "off",
+    "node/no-extraneous-import": "off",
+    "node/no-extraneous-require": "off",
+  },
 };
