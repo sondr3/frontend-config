495bf5e7798058681a46d0d4860ef3e5ea069dd5
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-02-22 19:12:05 +0100

    fix: eslint-config for React had wrong Prettier configuration

--- packages/eslint/src/plugins/react.js
+++ packages/eslint/src/plugins/react.js
@@ -5,7 +5,7 @@ module.exports = {
     },
   },
   plugins: ["react-hooks"],
-  extends: ["plugin:react/recommended", "prettier/react", "plugin:jsx-a11y/recommended"],
+  extends: ["plugin:react/recommended", "plugin:jsx-a11y/recommended"],
   rules: {
     "react-hooks/rules-of-hooks": "error",
     "react-hooks/exhaustive-deps": "warn",
