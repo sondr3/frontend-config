09d2fd9bd95695ecbdb6b30ba2ed3cb6d11ae0a5
Sondre Nilsen <nilsen.sondre@gmail.com>
2021-04-28 13:30:44 +0200

    chore: ignore unicorn/prefer-module rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
   extends: ["@sondr3/eslint-config/base", "@sondr3/eslint-config/node", "plugin:node/recommended-script"],
+  rules: {
+    "unicorn/prefer-module": "off",
+  },
 };
