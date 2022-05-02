99e2d41b7794df32f05028bea15e96bd7e687e90
Sondre Aasemoen <sondre@eons.io>
2022-05-02 10:29:55 +0200

    style: fix formatting for .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,9 @@
 module.exports = {
-  extends: ["@sondr3/eslint-config/base", "@sondr3/eslint-config/node", "plugin:node/recommended-script"],
+  extends: [
+    "@sondr3/eslint-config/base",
+    "@sondr3/eslint-config/node",
+    "plugin:node/recommended-script",
+  ],
   rules: {
     "unicorn/prefer-module": "off",
   },
