1bd51e59a13ea84eee8788e23b816c3c99a3d6d9
Sondre Aasemoen <sondre@eons.io>
2026-04-09 21:18:58 +0200

    Add @sondr3/oxfmt

---
+++ packages/oxfmt/index.ts
@@ -0,0 +1,14 @@
+import { OxfmtConfig } from "oxfmt"
+
+export const config: OxfmtConfig = {
+	ignorePatterns: ["**/__generated__/**", "**/__snapshots__/**", "**dist**", "**/coverage/**"],
+	endOfLine: "lf",
+	printWidth: 120,
+	tabWidth: 2,
+	useTabs: true,
+	sortPackageJson: true,
+	sortImports: {
+		internalPattern: ["~/", "#/"],
+		newlinesBetween: true,
+	},
+}
